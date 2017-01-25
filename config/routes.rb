98bd7c60aec1da23131af82be191b8d9a8463985
Jose Mena <josemena0114@gmail.com>
2017-01-25 12:40:38 +0000

    add new wiki path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
-
+  resources :wikis
+  
+  authenticated :user do
+    root 'wikis#index', as: :authenticated_root
+  end
   devise_for :users
   root to: 'welcome#index'
 end
