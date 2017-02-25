284257e0dbb61b6fe69b0cc8c1b177a69f8dbe50
Jose Mena <josemena0114@gmail.com>
2017-02-25 18:56:59 +0000

    charges controller created

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,13 @@
+class ChargesControllerController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edite
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'charges_controller/index'
+
+  get 'charges_controller/show'
+
+  get 'charges_controller/new'
+
+  get 'charges_controller/edite'
+
   resources :wikis
   
   authenticated :user do
