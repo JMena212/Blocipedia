394f05e8ce80abf62fec758ee38063a85628be78
Jose Mena <josemena0114@gmail.com>
2017-03-18 17:30:12 +0000

    remove collaborator function added

--- app/controllers/collaborations_controller.rb
+++ app/controllers/collaborations_controller.rb
@@ -13,7 +13,15 @@ class CollaborationsController < ApplicationController
     end
 
     def destroy
-        
+        u = User.find(params[:user_id])
+        w =  Wiki.find(params[:wiki_id])
+        c = Collaboration.find_by(user_id: u.id, wiki_id: w.id)
+        if c.destroy
+            flash[:notice]= 'Collaborator removed'
+            redirect_to edit_wiki_path(w)
+        else
+            flash[:alert]= 'Failed to remove collaboration'
+            redirect_to root_path
+        end
     end
-    
 end
